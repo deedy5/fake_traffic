60f1a0e5ee6292defeca269d4179e3f3871e02cf
deedy5 <65482418+deedy5@users.noreply.github.com>
2021-08-20 12:45:06 +0300

    v0.7
    1. threads added
    2. DEBUG parameter added
    3. some improvements

--- fake_traffic.py
+++ fake_traffic.py
@@ -1,4 +1,5 @@
 from random import uniform, choice, randint, sample
+from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 from time import sleep
 
@@ -8,7 +9,28 @@ from duckduckgo_search import ddg
 from google_searching import ggl
 from google_trends import realtime_trends
 
-__version__ = 0.6
+
+__version__ = 0.7
+
+
+THREADS = 1
+MIN_WAIT = 1
+MAX_WAIT = 30
+DEBUG = False
+
+BLACKLIST = ("https://t.co", "t.umblr.com", "messenger.com",
+             "itunes.apple.com", "l.facebook.com", "bit.ly",
+             "mediawiki", ".css", ".ico", ".xml", "intent/tweet",
+             "twitter.com/share", "signup", "login", "dialog/feed?",
+             ".json", ".svg", ".gif", "zendesk", "clickserve",
+             "mailto:", "smart-captcha/", "Login", "mail.google.com",
+             ".jpg", ".jpeg", ".png", ".iso", ".js", "s.click",
+             "javascript:", "whatsapp://", "tel:", "tg://", "/#",
+             "showcaptcha?")
+
+def debug_print(*agrs, **kwargs):
+    if DEBUG:
+        print(*agrs, **kwargs)
 
 def real_trends(country='US', language='en-US'):
     trends = realtime_trends(country=country, language=language, category='h', num_results=20)
@@ -16,17 +38,24 @@ def real_trends(country='US', language='en-US'):
 
 def get_url(url):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0"}
-    url = f"https://{url}" if ('https://' not in url) else url
+    if 'https://' not in url and 'http://' not in url:
+        url = f"https://{url}"
+    debug_print(url)
     try:
-        resp = requests.get(url, headers=headers, timeout=5)
+        resp = requests.get(url, headers=headers, timeout=4)
         if resp.status_code == 200:
-            print(resp.status_code, url)
+            debug_print(resp.url, resp.status_code)
+            if any(x in resp.url for x in BLACKLIST):
+                debug_print('URL in BLACKLIST')
+                return None            
             return resp
-    except requests.exceptions.ConnectionError:
-        print("Connection error. Sleep 30-60 sec.")
-        sleep(uniform(30, 60))
+        debug_print(resp.raise_for_status())      
+    except requests.ConnectionError:
+        debug_print("Connection error. Sleep 25-35 sec")
+        sleep(uniform(25, 35))
     except:
-        return None
+        debug_print("Error")
+
 
 def google_search(word, max_results=20):
     query  = word.replace(' ','+')
@@ -41,18 +70,11 @@ def ddg_search(word, max_results=20):
     return urls
 
 def parse_urls(response):
-    blacklist = ["https://t.co", "t.umblr.com", "messenger.com",
-                 "itunes.apple.com", "l.facebook.com", "bit.ly",
-                 "mediawiki", ".css", ".ico", ".xml", "intent/tweet",
-                 "twitter.com/share", "signup", "login", "dialog/feed?",
-                 ".json", ".svg", ".gif", "zendesk", "clickserve",
-                 "mailto:", "smart-captcha/", "Login", "mail.google.com",
-                 ".jpg", ".jpeg", ".png", ".iso",]
     try:
         tree = html.fromstring(response.text)
         tree.make_links_absolute(response.url)
         urls = tree.xpath('//a/@href')
-        urls = [url for url in urls if not any(x in url for x in blacklist)]
+        urls = [url for url in urls if not any(x in url for x in BLACKLIST)]
         return urls
     except:
         return []
@@ -66,37 +88,51 @@ def recursive_browse(url, depth=randint(0, 5)):
         recursive_urls = parse_urls(resp)
         if recursive_urls:
             url = choice(recursive_urls)
-            sleep(uniform(1, 30))
+            sleep(uniform(MIN_WAIT, MAX_WAIT))
             recursive_browse(url, depth-1)
+
+def _thread(trend):
+    word = ' '.join(sample(trend['entity_names'], 2))
+    article_urls = trend['article_urls']
+    print(f"*** Trend = {word} ***")
+       
+    google_urls, ddg_urls = [], []
+    try:
+        google_urls = google_search(word)
+        print(f"'{word}': google_urls len = {len(google_urls)}")
+        article_urls.extend(url for url in google_urls if url not in article_urls)
+    except:
+        print('google search error')
+    try:
+        ddg_urls = ddg_search(word)
+        print(f"'{word}': ddg_urls len = {len(ddg_urls)}")
+        article_urls.extend(url for url in ddg_urls if url not in article_urls)
+    except:
+        print('ddg search error')
+        
+    print(f"Recursive browsing {len(article_urls)} urls. Wait...", end='\n\n')    
+    for i, url in enumerate(article_urls, start=1):
+        debug_print(f"{i}/{len(article_urls)} recursive browse")
+        recursive_browse(url, depth=randint(max(0, 9-i), max(5, 11-i)))    
     
-def fake_traffic(country='US', language='en-US'):
+def fake_traffic(country='US', language='en-US', threads=THREADS, min_wait=MIN_WAIT, max_wait=MAX_WAIT, debug=DEBUG):
+    global THREADS, MIN_WAIT, MAX_WAIT, DEBUG
+    THREADS = threads
+    MIN_WAIT = min_wait
+    MAX_WAIT = max_wait
+    DEBUG = debug
+    
     print('*** Fake traffic ***')
     while True:
         print(f'\n{datetime.now()}')
-        print(f'------GET TRENDS IN {country=}------')
+        print(f'---GET TRENDS IN {country=} {language=}---')
         trends = real_trends(country=country, language=language)
-        trend = choice(trends)
-        word = ' '.join(sample(trend['entity_names'], 2))
-        article_urls = trend['article_urls']
-        print(f"*** Trend = {word} ***")
-        google_urls, ddg_urls = [], []
-        try:
-            google_urls = google_search(word)
-            print(f"'{word}': google_urls len = {len(google_urls)}")
-            article_urls.extend(url for url in google_urls if url not in article_urls)
-        except:
-            print('google search error')
-        try:
-            ddg_urls = ddg_search(word)
-            print(f"'{word}': ddg_urls len = {len(ddg_urls)}")
-            article_urls.extend(url for url in ddg_urls if url not in article_urls)
-        except:
-            print('ddg search error')
-        print(f"Recursive browsing {len(article_urls)} urls. Wait...")
-        for i, url in enumerate(article_urls, start=1):            
-            print(f"{i}/{len(article_urls)} urls recursive browsing...")
-            recursive_browse(url)
-        sleep(uniform(1, 10))
+        trends = sample(trends, threads)        
+        with ThreadPoolExecutor(threads) as executor:
+            for i, trend in enumerate(trends, start=1):
+                print(f"Thread {i} start.")
+                executor.submit(_thread, trend)
+                sleep(uniform(25, 35))      
 
 if __name__ == '__main__':
     fake_traffic(country='US', language='en-US')
