15625d7bb28b5c1d5d6dd934a504045654690f48
deedy5 <65482418+deedy5@users.noreply.github.com>
2025-01-18 19:21:58 +0300

    __init__: use __all__

--- fake_traffic/__init__.py
+++ fake_traffic/__init__.py
@@ -1,7 +1,6 @@
 import logging
 
-from .fake_traffic import FakeTraffic
-from .version import __version__
+__all__ = ["FakeTraffic", "__version__"]
 
 # A do-nothing logging handler
 # https://docs.python.org/3.3/howto/logging.html#configuring-logging-for-a-library
